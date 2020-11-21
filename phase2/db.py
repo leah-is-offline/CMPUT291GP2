aa98441a4daed102d8f1a5c128f4d7ea0354cf1a
leah-is-offline <copeland@ualberta.ca>
2020-11-21 12:56:06 -0800

    insertTag and insert post functions finished

--- phase2/db.py
+++ phase2/db.py
@@ -78,10 +78,40 @@ def insertTags(tags):
     """
     Inserts each tag in tags into the tags collection
     """
-    # TODO: implementation
-    for tag in tags:
-        pass
+    '''
+    When posting a question, the user will provide some tags.
+    If a user-provided tag exist in Tags collection, you will add one to the count field of the tag.
+    If a user-provided tag does not exist in Tags collection,
+    you will add it to the collection as a new row with a unique id and count 1'''
 
+    # TODO: implementation ---> Done after figuring out what those fields mean
+    
+    tags_coll = db["tags"]
+    for tag in tags:
+        check_tag_exists = tags_coll.count_documents({"TagName": tag})
+        if check_tag_exists > 0 :
+            #increase existing documents tag count by one
+            document = tags_coll.find_one_and_update(
+                {"TagName": tag},
+                {"$inc":{"Count":1}},
+                new=True
+            )
+            print("previous tag updated")
+            print(document)
+        else:
+            #create a new document for that tag
+            #TO DO: Find out what excerptPostID and WikiPostId is
+            tempDigit = -1
+            document = {
+                "Id": getNextId("tags"),
+                "TagName": str(tag),
+                "Count" : 1,
+                "ExcerptPostId": tempDigit,
+                "WikiPostId": tempDigit
+            }
+            db.tags.insert_one(document)
+            print("new post inserted")
+            print(document)
 
 
 def insertPost(title :str, body :str, tags :list, postType :str):
@@ -94,15 +124,15 @@ def insertPost(title :str, body :str, tags :list, postType :str):
 
     # only use valid tags
     tags = list(filter(lambda tag: len(tag) > 0, tags))
-
     insertTags(tags)
 
     stringTags = ""
     for tag in tags:
         stringTags += "<%s>" % tag
 
+
     currentDate = datetime.now()
-    currentISODate = my_date.isoformat()
+    currentISODate = currentDate.isoformat()
 
     document = {
         "Id": getNextId("posts"),
@@ -117,12 +147,15 @@ def insertPost(title :str, body :str, tags :list, postType :str):
         "FavoriteCount": 0,
         "ContentLicense": "CC BY-SA 4.0"
     }
-    
+
+    #if the post is a question, set answer count to zero
     if(postType == "1"):
         document["AnswerCount"] = 0
 
+    #record the user id if the user is not a guest
     if(not guestMode):
         document["OwnerUserId"] = currentuid
+
 
     db.posts.insert_one(document)
     print("new post inserted")
